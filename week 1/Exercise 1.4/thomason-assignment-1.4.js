da4a197ce1d1b443de334295fab1a71d284c5616
wthomason <williamthomason92@yahoo.com>
2018-11-29 21:13:38 -0600

    adding assignment 1.4

---
+++ week 1/Exercise 1.4/thomason-assignment-1.4.js
@@ -0,0 +1,32 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "William";
+var lastName = "Thomason";
+var age = "32";
+var street = "1112 63rd Street";
+var state = "IA";
+var zip = "50315";
+
+// output 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(state);
+console.log(zip); 
+
+
+// end program
