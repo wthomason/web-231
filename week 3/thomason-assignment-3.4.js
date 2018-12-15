2ea91e6a372f40e053fa1c1ab07eee026eb75111
wthomason <williamthomason92@yahoo.com>
2018-12-14 21:10:10 -0600

    added completed assignment 3.4 in week 3

--- week 3/thomason-assignment-3.4.js
+++ week 3/thomason-assignment-3.4.js
@@ -1,28 +1,46 @@
 /*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
-
-  FirstName LastName
-  Assignment 3.4
-  Today's Date
-
-  -- DO THE NUMBERS MATCH GAME --
-  6 does not match 4!
-  6 does not match 7!
-  6 does not match 4!
-  6 does not match 8!
-  6 does not match 9!
-  6 does not match 7!
-  6 does match 6!
-  6 does not match 3!
+============================================
+; Title:  Assignment 3.4
+; Author: William Thomason
+; Date:   13 December 2018
+; Description: loops and if/else statements
+;===========================================
 */
 
-/**
- * Params: n/a
- * Response: integer value
- * Description: Returns a random integer value
- * Disclaimer: Do not remove this function as this is what you will be using to generate a random number
- */
-function randomNumber() {
-  return Math.floor((Math.random() * 10) + 1)
+
+const header = require('../thomason-header.js');
+
+console.log(header.display("William", "Thomason", "Assignment 3.4"));
+
+console.log('\n')
+
+let num = 6;
+
+for (var i = 0; i < 10; i++) {
+
+  let num2 = Math.floor((Math.random() * 10) + 1)
+   match(num, num2)
+  if (match(num, num2)) {
+    logMatch(num, num2);
+  } else {
+    logMisMatch(num, num2);
+  }
+}
+
+function match(param1, param2) {
+  if (param1 === param2) {
+
+    return true
+  } else {
+
+    return false
+  }
+}
+
+function logMisMatch(param1, param2) {
+  console.log(param1 + " does not match " + param2 + '!');
+}
+
+function logMatch(param1, param2) {
+  console.log(param1 + " does match " + param2 + '!');
 }
