b618a7af1ca726eb66b019dd8d5b3885d9e39004
wthomason <williamthomason92@yahoo.com>
2018-12-19 21:40:49 -0600

    worked on assignment 4.4

--- week 4/thomason-assignment-4.4.js
+++ week 4/thomason-assignment-4.4.js
@@ -24,3 +24,46 @@
   Iowa
 
 */
+
+/*
+============================================
+; Title:  Assignment 4.4
+; Author: William Thomason
+; Date:   17 December 2018
+; Description: Arrays
+;===========================================
+*/
+
+//HEADER
+const header = require('../thomason-header.js');
+console.log(header.display("William", "Thomason", "Assignment 4.4") + "\n");
+
+//VARIABLES
+
+var states = ["Alabama", "Nebraska", "Iowa", "California", "Nevada"];
+
+//FUNCTIONS
+
+function getState(pram1, pram2){
+  if(pram1 === pram2){
+    return true;
+  }
+}
+
+function loopArray(arr){
+  for(var i=0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+//OUTPUT
+
+console.log("\n-- ORIGINAL ARRAY --");
+loopArray(states);
+
+console.log("\n-- SORTED ARRAY --");
+loopArray(states.sort());
+
+console.log("\n-- SELECTED VALUE --");
+
+//END PROGRAM
