5d61fcc85d3fcb540d3743ae9b50f8a75be0a88a
wthomason <williamthomason92@yahoo.com>
2019-01-01 08:51:10 -0600

    finished assignement 4.4

--- week 4/thomason-assignment-4.4.js
+++ week 4/thomason-assignment-4.4.js
@@ -1,31 +1,4 @@
 /*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
-
-  FirstName LastName
-  Assignment 4.4
-  Today's Date
-
-  -- ORIGINAL ARRAY --
-  Alabama
-  Nebraska
-  Iowa
-  California
-  Nevada
-
-  -- SORTED ARRAY --
-  Alabama
-  California
-  Iowa
-  Nebraska
-  Nevada
-
-  -- SELECTED VALUE --
-  Iowa
-
-*/
-
-/*
 ============================================
 ; Title:  Assignment 4.4
 ; Author: William Thomason
@@ -65,5 +38,7 @@ console.log("\n-- SORTED ARRAY --");
 loopArray(states.sort());
 
 console.log("\n-- SELECTED VALUE --");
-
+console.log(states.filter(function(el){
+  return getState(el,"Iowa")
+})[0]);
 //END PROGRAM
