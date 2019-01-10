844bf7479950d299cc0179bb37bb5b341172d4fd
wthomason <williamthomason92@yahoo.com>
2019-01-10 16:09:13 -0600

    updated discussion 5.1

--- week 5/thomason-discussion-5.1.js
+++ week 5/thomason-discussion-5.1.js
@@ -3,10 +3,26 @@
 ; Title:  Discussion 5.1
 ; Author: William Thomason
 ; Date:   9 January 2019
-; Description: Built in Functions
+; Description: Collection Array with errors
 ;===========================================
 */
 
+/*
+
+Expected Output
+
+-- Classic Cars List --
+thomason-discussion-5.1-no-errors.js:72
+Year: 1964, Make: Chevy, Model: Impala
+thomason-discussion-5.1-no-errors.js:74
+Year: 1981, Make: DMC, Model: Delorean
+thomason-discussion-5.1-no-errors.js:74
+Year: 1989, Make: Porsche, Model: 911
+thomason-discussion-5.1-no-errors.js:74
+Year: 1966, Make: Chevy, Model: Camaro
+thomason-discussion-5.1-no-errors.js:74
+Year: 1934, Make: Bugatti, Model: Type 57
+*/
 //HEADER
 const header = require('../thomason-header.js');
 console.log(header.display("William", "Thomason", "Discussion 5.1") + "\n");
