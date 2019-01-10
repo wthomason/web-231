9c7d4d1dc7794b076d7680fe40bd75d1702943e4
wthomason <williamthomason92@yahoo.com>
2019-01-09 21:46:33 -0600

    completed exercise 5.3

--- week 5/thomason-exercise-5.3.js
+++ week 5/thomason-exercise-5.3.js
@@ -11,18 +11,75 @@
 const header = require('../thomason-header.js');
 console.log(header.display("William", "Thomason", "Exercise 5.3") + "\n");
 
+//VARIABLES
+var composerList  = [
 
-/*
-  Expected output:
+  {
 
-  FirstName LastName
-  Exercise 5.3
-  Today's Date
+  firstName: 'Ludwig',
 
-  -- COMPOSERS --
-  Last Name: Beethoven, Genre: Classical, Rating: 8
-  Last Name: Mozart, Genre: Classical, Rating: 10
-  Last Name: Bach, Genre: Classical, Rating: 9
-  Last Name: Haydn, Genre: Classical, Rating: 6
-  Last Name: Schubert, Genre: Classical, Rating: 5
-*/
+  lastName: 'Beethoven',
+
+  genre: 'Classical',
+
+  rating: 7,
+
+  },
+
+  {
+
+  firstName: 'Wolfgang',
+
+  lastName: 'Mozart',
+
+  genre: 'Classical',
+
+  rating: 4,
+
+  },
+
+  {
+
+  firstName: 'Johann',
+
+  lastName: 'Bach',
+
+  genre: 'Classical',
+
+  rating: 9,
+
+  },
+
+  {
+
+  firstName: 'Joseph',
+
+  lastName: 'Haydn',
+
+  genre: 'Classical',
+
+  rating: 6,
+
+  },
+
+  {
+
+  firstName: 'Franz',
+
+  lastName: 'Schubert',
+
+  genre: 'Classical',
+
+  rating: 8,
+
+  },
+
+  ];
+
+  //OUTPUT
+console.log("-- COMPOSERS --");
+composerList.forEach(function(composer){
+  console.log("First Name: " + composer.firstName + ", " + "Last Name: " + composer.lastName + ", " + "Genre: " + composer.genre + ", " + "Rating: " + composer.rating);
+});
+
+  //END PROGRAM
