5ecd3611802122a6cf4a7fc003943d475629e525
wthomason <williamthomason92@yahoo.com>
2019-01-18 21:09:37 -0600

    worked on assignment-6.4

--- week 6/thomason-assignment-6.4.js
+++ week 6/thomason-assignment-6.4.js
@@ -8,3 +8,36 @@
 
   Ticket 105 was created on <today' date> and assigned to employee Bob Jones (Programmer I).
 */
+/*
+============================================
+; Title:  Assignment 6.4
+; Author: William Thomason
+; Date:   16 January 2019
+; Description: Exception Handling
+;===========================================
+*/
+
+//HEADER
+const header = require('../thomason-header.js');
+console.log(header.display("William", "Thomason", "Assignment 6.4") + "\n");
+
+//VARIABLES
+
+var ticket = {
+  id: 105,
+  name: "Web Spec",
+  dateCreated: new Date().toLocaleDateString('en-US'),
+  priority: "High",
+
+  person: {
+    id: 152,
+    firstName: "Bob",
+    lastName: "Jones",
+    jobTitle: "Programmer I",
+  }
+}
+
+//OUTPUT
+console.log(ticket.dateCreated);
+
+//END PROGRAM
