67e9a72047a74ce2d47989fa3684d6a710d200ec
wthomason <williamthomason92@yahoo.com>
2019-01-19 07:38:51 -0600

    finished assignment-6.4

--- week 6/thomason-assignment-6.4.js
+++ week 6/thomason-assignment-6.4.js
@@ -1,14 +1,4 @@
 /*
-  // Below is what I am expecting to see when I run your program - remove this before submitting your work
-  // 50% of you will not read this and lose 25% of the awardable points.
-
-  FirstName LastName
-  Assignment 6.4
-  Today's Date
-
-  Ticket 105 was created on <today' date> and assigned to employee Bob Jones (Programmer I).
-*/
-/*
 ============================================
 ; Title:  Assignment 6.4
 ; Author: William Thomason
@@ -38,6 +28,6 @@ var ticket = {
 }
 
 //OUTPUT
-console.log(ticket.dateCreated);
+console.log("Ticket " + ticket.id + " was created on " + ticket.dateCreated + " and assigned to employee " + ticket.person.firstName + " " + ticket.person.lastName + " (" + ticket.person.jobTitle + ").");
 
 //END PROGRAM
