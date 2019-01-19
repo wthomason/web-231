b97f3f8384b6b7498c8d7889bf80d40a7040e68e
wthomason <williamthomason92@yahoo.com>
2019-01-18 20:54:50 -0600

    finished exercise-6.3

--- week 6/thomason-exercise-6.3.js
+++ week 6/thomason-exercise-6.3.js
@@ -1,9 +1,28 @@
 /*
-  Expected output:
-
-  FirstName LastName
-  Exercise 6.3
-  Today's Date
-
-  {id: 101, name: Help Desk Support, requester: Bob Jones}
+============================================
+; Title:  Exercise 6.3
+; Author: William Thomason
+; Date:   16 January 2019
+; Description: Object Literals
+;===========================================
 */
+
+//HEADER
+const header = require('../thomason-header.js');
+console.log(header.display("William", "Thomason", "Exercise 6.3") + "\n");
+
+//OBJECT ticket with the id, name and requester
+var ticket = {
+
+  id: 165,
+
+  name: "Will Call at booth number 6",
+
+  requester: "William T. Thomason",
+
+};
+
+//OUTPUT
+console.log("{id: " + ticket.id, ", name: " + ticket.name + ", requester: " + ticket.requester + "}");
+
+//END PROGRAM
